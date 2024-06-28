e7012019da9bb5fc167733a75527b527fc31fd31
ZekeGBlox <157068979+ZekeGBlox@users.noreply.github.com>
2024-06-28 14:18:52 +0100

    Update luarrmourtest.lua

--- luarrmourtest.lua
+++ luarrmourtest.lua
@@ -1,3 +1,4 @@
+print("just updated")
 print("yeah maybe")
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/52e8708db1b35f85f5a3f8166c41a0e8.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9aacf995d22cba738c8caf843b84285f.lua"))()
 print("can it actually fucking run!?!??!?!")
