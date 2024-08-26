d6f3d7d4037557a82dbc1399e261c2cf3310c9df
ZekeGBlox <157068979+ZekeGBlox@users.noreply.github.com>
2024-08-26 17:44:04 +0100

    Delete luarrmourtest.lua

--- luarrmourtest.lua
+++
@@ -1,4 +0,0 @@
-print("just updated")
-print("yeah maybe")
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9aacf995d22cba738c8caf843b84285f.lua"))()
-print("can it actually fucking run!?!??!?!")
